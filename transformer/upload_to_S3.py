71b8fdabcc2cd51a7d74f621f011740b014d1fd3
jjangddu <peterjdw@naver.com>
2025-04-21 20:39:59 +0900

    final code

--- transformer/upload_to_S3.py
+++ transformer/upload_to_S3.py
@@ -34,24 +34,21 @@ def upload_file_to_s3(file_name, bucket, object_name=None):
 
 # 업로드할 파일 목록
 files_to_upload = [
-    "BTC-USD_data.json"
-    "GOOGL_data.json"
+    # "BTC-USD_data.json"
+    "GOOGL_data.json",
+    "GOOGL_prediction.json",
     "AAPL_data.json",
     "AAPL_prediction.json",
     "NVDA_data.json",
     "NVDA_prediction.json",
-    "TSLA_data.json"
+    "TSLA_data.json",
+    "TSLA_prediction.json",
     # "stocks.json"
     "AAPL_investor_results.json",
     "GOOGL_investor_results.json",
     "NVDA_investor_results.json",
     "TSLA_investor_results.json"
 ]
-
-    files_to_upload.append(f"AAPL_prediction_feature.json")
-    files_to_upload.append(f"NVDA_prediction_feature.json")
-    files_to_upload.append(f"GOOGL_prediction_feature.json")
-    files_to_upload.append(f"BTC-USD_prediction_feature.json")
 
 print(files_to_upload)
 
